0ac9b095c7e10572d2b2a4613c119dd0d5045c02
jackiecrazy <jackiecrazyhi@gmail.com>
2023-11-23 16:22:34 -0800

    aether bug fix

--- src/main/java/jackiecrazy/footwork/client/ClientRegisters.java
+++ src/main/java/jackiecrazy/footwork/client/ClientRegisters.java
@@ -4,6 +4,7 @@ import jackiecrazy.footwork.Footwork;
 import jackiecrazy.footwork.client.particle.CustomSweepParticle;
 import jackiecrazy.footwork.client.particle.FootworkParticles;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,5 +21,11 @@ public class ClientRegisters {
         e.registerSpriteSet(FootworkParticles.CIRCLE.get(), set -> new CustomSweepParticle.SweepProvider(set, CustomSweepParticle.ROTATIONTYPE.SUPERFLAT));
         e.registerSpriteSet(FootworkParticles.SWEEP_LEFT.get(), set -> new CustomSweepParticle.SweepProvider(set, CustomSweepParticle.ROTATIONTYPE.NORMAL));
         e.registerSpriteSet(FootworkParticles.BONK.get(), CustomSweepParticle.BonkProvider::new);
+    }
+
+
+    @SubscribeEvent
+    public static void keys(final RegisterKeyMappingsEvent event) {
+        event.register(Keybinds.SELECT);
     }
 }
